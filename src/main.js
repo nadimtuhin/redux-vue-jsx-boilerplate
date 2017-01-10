03758dfc46f1d665b7624006a451a8b5b76b465a
Nadim Tuhin <nadimtuhin@gmail.com>
2017-01-10 19:23:10 +0600

    installed store in main js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@
 import { reduxStorePlugin } from 'redux-vue';
 import Vue from 'vue';
 import App from './App';
+import store from './store';
 
 // install redux-vue
 Vue.use(reduxStorePlugin);
 
 /* eslint-disable no-new */
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app');
