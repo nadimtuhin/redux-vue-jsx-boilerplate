beb841a2f978b5359672ce19fae06832cf5df9bb
Nadim Tuhin <nadimtuhin@gmail.com>
2017-01-10 19:11:47 +0600

    installed redux-vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import { reduxStorePlugin } from 'redux-vue';
+import Vue from 'vue';
+import App from './App';
+
+// install redux-vue
+Vue.use(reduxStorePlugin);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+  render: h => h(App)
+}).$mount('#app');
